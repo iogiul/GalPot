cbbbb6cb87a751952e3d031ef40087d5834f7326
iogiul <giuliano.iorio89@gmail.com>
2018-10-04 23:37:44 +0100

    A

--- Orbit_parallel_worbit_sample.cc
+++ Orbit_parallel_worbit_sample.cc
@@ -15,6 +15,7 @@
 #include <ctime>
 #include <omp.h>
 #include <iomanip>
+#include <sstream>
 
 int main(int argc,char *argv[])  
 {
@@ -123,19 +124,22 @@ int main(int argc,char *argv[])
     }
     data.close();
     
+    std::istringstream ss(argv[4]);
+    int nOut;
+    ss>>nOut;
     
-    
-    int nOut=100;
+
     int IntegrationFail;
     Vector<double,7> *OVL = new Vector<double,7> [nData];
-    Vector<double,11> *EVL = new Vector<double,11> [nData];
-    Vector<double, 7> *PSL = new Vector<double,7> [nData*nOut];
+    Vector<double,12> *EVL = new Vector<double,12> [nData];
+    Vector<double, 8> *PSL = new Vector<double,8> [nData*nOut];
     OrbitIntegratorWithStats* OIVEC= new  OrbitIntegratorWithStats[nData];
     
     
     
    
     // read file
+    std::cout<<"Norbits  "<<nOut <<std::endl;
     std::cout<<"Wait..... stars are orbiting/"<<std::endl;
     OrbitIntegratorWithStats OI;
     
@@ -149,7 +153,7 @@ int main(int argc,char *argv[])
     Vector<double,6> *OrbOut = new Vector<double,6>[nOut];
 
     Potential *Phic = new GalaxyPotential(nDisk, Dpar, nSphr, Spar);
-	double L,Lz,LR,LT,inc,incmax,incmin,incmean,Lzmin,Lzmax,Lmin,Lmax;
+	double L,Lz,LR,LT,inc,incmax,incmin,incmean,Lzmin,Lzmax,Lmin,Lmax,thetamax;
 	int iini, ifin;
 		
 	
@@ -187,9 +191,11 @@ int main(int argc,char *argv[])
 				
 				//Save orbits stuff
 				PSL[iini+j][0]=i;
+				PSL[iini+j][7]=tOut[j];
 				for(int k=1; k!=7; k++){
 					PSL[iini+j][k]=OrbOut[j][k-1];
 				}
+					
 				
 				
             }
@@ -197,19 +203,21 @@ int main(int argc,char *argv[])
 			incmean=(incmean/nOut)*torad;
 			incmin=incmin*torad;
 			incmax=incmax*torad;
+			thetamax=atan(OI.Maxz/OI.MaxR)*torad;
 			
             OVL[i][6]=tOut[nOut-1];
-			EVL[i][0]=OI.Minr/Units::kpc; //rperi
+		EVL[i][0]=OI.Minr/Units::kpc; //rperi
 	        EVL[i][1]=OI.Maxr/Units::kpc; //rapo
 	        EVL[i][2]=OI.MinR/Units::kpc; //MaxR
 	        EVL[i][3]=OI.MaxR/Units::kpc; //MinR
-			EVL[i][4]=OI.Maxz/Units::kpc; //zmax
+		EVL[i][4]=OI.Maxz/Units::kpc; //zmax
 	        EVL[i][5]=(OI.Maxr-OI.Minr)/(OI.Maxr+OI.Minr); //ecc
 	        EVL[i][6]=OI.Energy/(Units::kms*Units::kms);  //E
 	        EVL[i][7]=OI.Lz/(Units::kpc*Units::kms); //Lz
 	        EVL[i][8]=incmin; //imin
 	        EVL[i][9]=incmax; //imax
 	        EVL[i][10]=incmean; //imean
+	        EVL[i][11]=thetamax; //thetamax
 		} else {
 				
 				for(int j=0;j!=nOut;j++){
@@ -231,6 +239,7 @@ int main(int argc,char *argv[])
 		        EVL[i][8]=1.e10;
 		        EVL[i][9]=1.e10;
 		        EVL[i][10]=1.e10;
+		        EVL[i][11]=1.e10;
 			
 		}
 			
@@ -250,7 +259,7 @@ int main(int argc,char *argv[])
     
     // Write header
     //output << "#R z Phi VR Vz Vphi T  Rini   zini Phiini VRini Vzini Vphiini \n" << std::flush;
-    output << "#0-id 1-rmin 2-rmax 3-Rmin 4-Rmax 5-zmax 6-ecc 7-E 8-Lz 9-incmin 10-incmax 11-incmean \n" << std::flush;
+    output << "#0-id 1-rmin 2-rmax 3-Rmin 4-Rmax 5-zmax 6-ecc 7-E 8-Lz 9-incmin 10-incmax 11-incmean 12-thetamax \n" << std::flush;
     
 	//Output properties
     for (int i=0; i<nData; i++) {
@@ -268,6 +277,7 @@ int main(int argc,char *argv[])
 		<< EVL[i][8]  << ' '  //incin
 		<< EVL[i][9]  << ' '  //incmax
 		<< EVL[i][10]  << ' '  //incmean
+		<< EVL[i][11]  << ' '  //thetamax
         << std::endl << std::flush;
          
         //output<< EVL[i][2] << std::endl << std::flush;
@@ -278,9 +288,11 @@ int main(int argc,char *argv[])
 	
 	
 	outputor.open(argv[3]);
-	outputor << "#0-id 1-R 2-z 3-Phi 4-VR 5-Vz 6-Vphi \n" << std::flush;
-		
+	outputor << "#0-id 1-R 2-z 3-Phi 4-VR 5-Vz 6-Vphi 7-t \n" << std::flush;
+	
+	int j;	
 	for (int i=0; i<nData*nOut; i++) {
+	    j=floor(i/nOut);
             outputor
         	<<PSL[i][0]  << ' ' //id
         	<< PSL[i][1] / Units::kpc  << ' '   //R
@@ -288,7 +300,20 @@ int main(int argc,char *argv[])
         	<< PSL[i][3] / Units::degree  << ' '   //Phi
         	<< PSL[i][4] / Units::kms  << ' '   //VR
         	<< PSL[i][5] / Units::kms << ' '   //Vz
-			<< PSL[i][6] / Units::kms  << ' '   //VPhi
+		<< PSL[i][6] / Units::kms  << ' '   //VPhi
+		<< PSL[i][7]  / Units::Gyr << ' '   //time
+		<< EVL[j][0] << ' '  //rperi
+        	<< EVL[j][1] << ' '  //rapo
+        	<< EVL[j][2] << ' '  //MinR
+        	<< EVL[j][3]  << ' ' //MaxR
+        	<< EVL[j][4]  << ' '  //zmax
+        	<< EVL[j][5] << ' '  //ecc
+        	<< EVL[j][6]  << ' ' //E
+		<< EVL[j][7]  << ' '  //Lz
+		<< EVL[j][8]  << ' '  //incin
+		<< EVL[j][9]  << ' '  //incmax
+		<< EVL[j][10]  << ' '  //incmean
+		<< EVL[j][11]  << ' '  //thetamax
         	<< std::endl << std::flush;
 				
 	}
